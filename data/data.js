9e750840d556c238c5516c16b74e55e7ea137a00
ArcieeX <Arciee.herrera@gmail.com>
2024-08-23 14:11:57 -0700

    initial commit

--- data/data.js
+++ data/data.js
@@ -8,13 +8,13 @@ const projectData = [
     {
         title: "Farmers Market",
         image:"imgs/farmers-market.png",
-        description: "Second Website Project",
+        description: "Second Website Project.",
         link: "https://arcieex.github.io/cannons-coffee-website/",
     },
     {
         title: "Form",
         image:"imgs/form1.png",
-        description: "This form was created 6/15/2023 on Visual Studios. Structured with HTML5, CSS flexbox& Grid And JavaScipt  ",
+        description: "This form was created 6/15/2023 on Visual Studios. Structured with HTML5, CSS flexbox& Grid And JavaScipt.  ",
         link: "https://arcieex.github.io/Ice-Cream-Form/",
     },
 ]
--- js/custom.js
+++ js/custom.js
@@ -4,62 +4,88 @@ console.log(projectData)
 const nav = document.querySelector(".sidebar");
 const icon = document.querySelector(".pink-menu i");
 
-
-
-
 icon.addEventListener("click", showNav);
 function showNav() {
   icon.classList.toggle("fa-times")
   nav.classList.toggle("change");
+  // nav.classList.toggle("display-none")
+  // nav.classList.add("change")
 };
-showNav()
+// showNav()
 
+//NavBar starts hidden (displau: none)
+//When the user clicks the button, the nav shows
 
 
 let cards = document.getElementById('cards-wrapper');
-if (cards != null) {
-  let projects = projectData;
-  function showCards(projects, cards) {
 
-    for (let item of projects) {
-      let card = document.createElement("div")
-      card.classList.add("content")
-      card.innerHTML = `<h2>${item.title}</h2>
+function showCards(projects, cards) {
+
+  for (let item of projects) {
+    let card = document.createElement("div")
+    card.classList.add("content")
+    card.innerHTML = `<h2>${item.title}</h2>
     <img src=${item.image}>
     <p>${item.description}</p>
     <a href="${item.link}">Click Here</a>`
-      cards.appendChild(card)
-    }
+    cards.appendChild(card)
   }
+}
+
+if (cards != null) {
+  let projects = projectData;
   showCards(projects, cards);
+};
 
 
 
-  let cardsBG = document.getElementById("cardbg");
-  cardsBG.addEventListener("click", changeColor);
-  function changeColor() {
+let hideShow = document.querySelector('.show-div');
 
-    let selectColor =
-      document.getElementById("selectColor").value;
+const toggleButton = document.getElementById('toggleBtn');
+toggleButton.addEventListener('click', toggleDiv);
 
-    // From the drop-down menu, obtain 
-    // the value of the chosen color.
-    let selectedColor =
-      selectColor.options[selectColor.selectedIndex].value;
+function toggleDiv() {
+  hideShow.classList.toggle('display-none')
+};
+let color = "#E99394";
+let textColor = "#b36a5eff";
 
-    // Set the font color of the "gfg" 
-    // element to the selected color
-    cardsBG.style.color = selectedColor;
-    cardsBG.style.backgroundColor = selectedColor;
-  };
+let userArray = [];
+const form = document.getElementById("form");
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+	event.preventDefault();
+	const data = new FormData(event.target);
+	const dataObject = Object.fromEntries(data.entries());
+	//above is boiler plate code
+	userArray.push(dataObject);
+  changeColor(color);
+  changeTextColor(textColor);
+	form.reset();
+};
 
 
+let select = document.querySelector("select");
+  select.addEventListener("change", (event) => {
+    color = event.target.value;
+   
+    
+  });
+let coloredText = document.querySelector("label");
+  coloredText.addEventListener("change", (event) => {
+    color = event.target.value;
+   
+    
+  });
 
 
+function changeColor(value) {
+  console.log(value)
+  hideShow.style.backgroundColor = value;
+};
+function changeTextColor(value){
+  coloredText.style.color = value;
+}
 
-// let arrow = document.getElementById('scroll');
-// arrow.addEventListener('click', scrollUp);
-// function scrollUp() {
-
-// }
 
